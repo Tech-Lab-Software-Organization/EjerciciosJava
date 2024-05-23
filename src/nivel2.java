2f9e7dffa1bc9bc512c5c70d49b1dc39ec609d2a
jose ceron <cr24004@esfe.agape.edu.sv>
2024-05-22 20:54:53 -0600

    pregunta 4 lista

--- src/nivel2.java
+++ src/nivel2.java
@@ -1,6 +1,23 @@
-public class nivel2{
+import java.util.Scanner;
+
+public class nivel2 {
+    public int puntaje;
+    private String respuesta;
+    private final String[] animales;
+    private final Scanner ent;
+
+    public nivel2() {
+        puntaje = 0;
+        ent = new Scanner(System.in);
+        animales = new String[4];
+        animales[4] = "elefante";
+      
+    }
     
     public void imprimePregunta() {
+        System.out.println("Es un animal grande que tiene una trompa");
+        respuesta = ent.nextLine();
+        puntajeElefante();
         
     }
     
@@ -15,8 +32,16 @@ public class nivel2{
     public void pregunta3() {
         
     }
-
-    public void pregunta4() {
         
+    private void puntajeElefante() {
+        if (respuesta.equals(animales[4])) {
+            System.out.println("Correcto, haz ganado 10 puntos");
+            puntaje += 10;
+        } else {
+            System.out.println("Incorrecto. La respuesta es: " + animales[4]);
+            System.out.println("Has perdido 5 puntos");
+            puntaje -= 5;
+        }
     }
 }
+
