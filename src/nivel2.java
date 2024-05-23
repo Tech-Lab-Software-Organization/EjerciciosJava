3cadc66c18c983bf141e8619395db12d561ec082
Hillary Yeraldy Carpio Morales <2430933@clases.edu.sv>
2024-05-22 23:00:35 -0600

    el metodo pregunta 1 (Hillary Yeraldy Carpio Morales)

--- src/nivel2.java
+++ src/nivel2.java
@@ -12,39 +12,50 @@ public class nivel2 {
         animales = new String[4];
         animales[4] = "elefante";
         animales[5] = "tigre";
-      
+        animales[3] = "hormiga";
+
     }
-    
+
     public void imprimePregunta() {
         System.out.println("Es un animal grande que tiene una trompa");
         respuesta = ent.nextLine();
         puntajeElefante();
 
-        System.out.println( "Es un felino de franjas negras y naranjas");
+        System.out.println("Es un felino de franjas negras y naranjas");
         respuesta = ent.nextLine();
         pregunta3();
-        
+        System.out.println("Es un animal pequeño");
+        respuesta = ent.nextLine();
+        hormiga();
+
     }
-    
-    public void pregunta1 () {
-        
+
+    public void hormiga() {
+        if (respuesta.equals(animales[3])) {
+            System.out.println("Correcto has ganado una hormiga mas 10 puntos");
+            puntaje += 10;
+        } else {
+            System.out.println("incorrecto la respuesta es: " + animales[3]);
+            System.out.println("has perdido una hormiga y 5 puntos");
+            puntaje += 10;
+        }
     }
 
     public void pregunta2() {
-        
+
     }
 
     public void pregunta3() {
         if (respuesta.equals(animales[5])) {
             System.out.println("Correcto, haz ganado 20 puntos");
             puntaje += 20;
-            } else {
+        } else {
             System.out.println("Incorrecto. La respuesta es: " + animales[5]);
             System.out.println("Has perdido 10 puntos");
             puntaje -= 10;
-            }
+        }
     }
-        
+
     private void puntajeElefante() {
         if (respuesta.equals(animales[4])) {
             System.out.println("Correcto, haz ganado 10 puntos");
